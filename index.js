f4db1102e0566b542cff9c20f559f4122cf6b80a
Hari Basnet <harikrishnabsnt@gmail.com>
2025-03-10 22:44:43 +0200

    something has been changed

--- index.js
+++ index.js
@@ -1,80 +1,81 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
-const app = express()
-const PORT = process.env.PORT || 3001
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Person = require("./models/person");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json())
-app.use(morgan('tiny'))
-app.use(cors())
-app.use(express.static('dist'))
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.static("dist"));
 
 // custom method for logging POST request content
-morgan.token('req-body', (req) => {
-  if (req.method === 'POST') {
-    return JSON.stringify(req.body)
+morgan.token("req-body", (req) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
   }
-  return ''
-})
+  return "";
+});
 
-// log
+// logging
 app.use(
   morgan(
-    ':method :url :status :res[content-length] - :response-time ms :req-body'
+    ":method :url :status :res[content-length] - :response-time ms :req-body"
   )
-)
+);
 
-app.get('/api/persons', (request, response, next) => {
+app.get("/api/persons", (request, response, next) => {
   Person.find({})
     .then((persons) => {
-      response.json(persons)
+      response.json(persons);
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.get('/api/persons/:id', (request, response, next) => {
-  const id = request.params.id
+app.get("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
   Person.findById(id)
     .then((result) => {
-      response.json(result)
+      response.json(result);
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.post('/api/persons', (request, response, next) => {
-  const { name, number } = request.body
+app.post("/api/persons", (request, response, next) => {
+  const { name, number } = request.body;
 
-  const foundPerson = Person.find({ name: name }).then((result) => result)
+  const foundPerson = Person.find({ name: name }).then((result) => result);
 
-  console.log(foundPerson)
+  console.log(foundPerson);
   const person = new Person({
     name: name,
     number: number,
-  })
+  });
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson)
+      response.json(savedPerson);
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.delete('/api/persons/:id', (request, response, next) => {
-  const { id } = request.params
+app.delete("/api/persons/:id", (request, response, next) => {
+  const { id } = request.params;
   Person.findByIdAndDelete(id)
     .then((result) => {
-      response.status(200).send(result)
+      response.status(200).send(result);
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.put('/api/persons/:id', (request, response, next) => {
-  const { id } = request.params
-  const { name, number } = request.body
-  const { query } = request
+app.put("/api/persons/:id", (request, response, next) => {
+  const { id } = request.params;
+  const { name, number } = request.body;
+  const { query } = request;
 
   Person.findByIdAndUpdate(
     id,
@@ -83,40 +84,40 @@ app.put('/api/persons/:id', (request, response, next) => {
   )
     .then((updatedPerson) => {
       if (updatedPerson) {
-        response.json(updatedPerson)
+        response.json(updatedPerson);
       } else {
-        response.status(404).end()
+        response.status(404).end();
       }
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
-app.get('/info', (request, response, next) => {
-  const currentDate = new Date()
+app.get("/info", (request, response, next) => {
+  const currentDate = new Date();
   Person.find({})
     .then((persons) => {
       response.json(
         `Phonebook has infor for ${persons.length} people. ${currentDate}`
-      )
+      );
     })
-    .catch((error) => next(error))
-})
+    .catch((error) => next(error));
+});
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
+  next(error);
+};
 
 // this has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log('App listening in port', PORT)
-})
+  console.log("App listening in port", PORT);
+});
